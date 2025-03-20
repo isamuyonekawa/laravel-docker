357c1db2f1cd4531d628385b2b5bf42c5fed1cc7
isamuyonekawa <8orange.pp@gmail.com>
2025-03-20 13:55:07 +0900

    c

--- app/Http/Controllers/ValidateController.php
+++ app/Http/Controllers/ValidateController.php
@@ -17,6 +17,15 @@ class ValidateController extends Controller
         $request->validate([
             'name'  => ['required', 'min:3', 'max:10'],
             'email' => ['required', 'email'],
+        ], [
+            'name.required' => '名前は必須です。',
+            'name.min'      => '名前は3文字以上で入力してください。',
+            'name.max'      => '名前は10文字以内で入力してください。',
+            'email.required' => 'メールアドレスは必須です。',
+            'email.email'    => 'メールアドレスの形式で入力してください。',
+        ], [
+            'name'  => '名前',
+            'email' => 'メールアドレス',
         ]);
 
         dd('Validation Success');
