0dfd0d8a607d1ebc9fd5f72d17cfb45a77e0f0b6
isamuyonekawa <8orange.pp@gmail.com>
2025-04-02 23:09:13 +0900

    シーディング

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        // $users = User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // foreach ($users as $user) {
+        //     Post::factory(random_int(2, 5))->create([
+        //         'user_id' => $user,
+        //     ]);
+        // }
+
+        [$me] = User::factory(10)->create()->each(function ($user) {
+            Post::factory(random_int(2, 5))->create([
+                'user_id' => $user,
+            ]);
+        });
+
+        $me->update([
+            'name' => 'me',
+            'email' => 'me@exsample.com',
+            'password' => Hash::make('password'),
         ]);
     }
 }
